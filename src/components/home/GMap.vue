749ace622f1c9cd282e19d8bf073960ee2de0feb
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-10 16:15:42 -0500

    google map added to project

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div class="gmap" id="map"></div>
   </div>
 </template>
 
@@ -8,11 +8,37 @@
 export default {
   name: "GMap",
   data() {
-    return {};
+    return {
+      lat: 40,
+      lng: -74
+    };
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetVieControl: false
+      });
+    }
+  },
+  mounted() {
+    this.renderMap();
   }
 };
 </script>
 
 <style>
+.gmap {
+  width: 100%;
+  height: 100%;
+  background: gray;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: -1;
+}
 </style>
 
