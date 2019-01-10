b0a00ab7f503b62dd8e022dc32d4158ce43f9632
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-10 15:08:52 -0500

    firebase installed and added to project

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC2LLsG0aa7yFE9n9OUpd1wAOeAmBujevE",
+    authDomain: "vue-geo-app-4d44d.firebaseapp.com",
+    databaseURL: "https://vue-geo-app-4d44d.firebaseio.com",
+    projectId: "vue-geo-app-4d44d",
+    storageBucket: "vue-geo-app-4d44d.appspot.com",
+    messagingSenderId: "79596042169"
+};
+const firebaseApp = firebase.initializeApp(config);
+export default firebaseApp.firestore()
