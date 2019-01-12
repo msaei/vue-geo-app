e3bb1237c93c66e33ff16e065911d22ca7bec97c
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-12 00:27:52 -0500

    geolocation grabbed and set

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -27,8 +27,23 @@ export default {
     }
   },
   mounted() {
-    this.renderMap();
-    console.log(firebase.auth().currentUser);
+    // get user geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          this.lat = pos.coords.latitude;
+          this.lng = pos.coords.longitude;
+          this.renderMap();
+        },
+        err => {
+          console.log(err);
+          this.renderMap();
+        },
+        { maximumAge: 60000, timeout: 3000 }
+      );
+    } else {
+      this.renderMap();
+    }
   }
 };
 </script>
