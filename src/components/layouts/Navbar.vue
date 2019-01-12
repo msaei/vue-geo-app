0073a31e5ea426b30d2ce5b9edd69513938fbd6d
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-12 11:24:55 -0500

    conditional navbar

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -5,13 +5,13 @@
         <router-link :to="{name:'GMap'}" class="brand-logo left">GeoChat</router-link>
         <ul class="right">
           <li>
-            <router-link :to="{ name: 'Signup'}">Signup</router-link>
+            <router-link :to="{ name: 'Signup'}" v-if="!user">Signup</router-link>
           </li>
           <li>
-            <router-link :to="{name: 'Login'}">Login</router-link>
+            <router-link :to="{name: 'Login'}" v-if="!user">Login</router-link>
           </li>
           <li>
-            <a @click="logout">Logout</a>
+            <a @click="logout" v-if="user">Logout</a>
           </li>
         </ul>
       </div>
@@ -24,7 +24,9 @@ import firebase from "firebase";
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      user: null
+    };
   },
   methods: {
     logout() {
@@ -35,6 +37,11 @@ export default {
           this.$router.push({ name: "Login" });
         });
     }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user;
+    });
   }
 };
 </script>
