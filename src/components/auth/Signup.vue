2540bc5424560be5896296b6a01dcdca8b8dc095
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-11 22:33:09 -0500

    signup works properly

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -54,6 +54,16 @@ export default {
             firebase
               .auth()
               .createUserWithEmailAndPassword(this.email, this.password)
+              .then(user => {
+                ref.set({
+                  alias: this.alias,
+                  geolocation: null,
+                  user_id: user.uid
+                });
+              })
+              .then(() => {
+                this.$router.push({ name: "GMap" });
+              })
               .catch(err => {
                 console.log(err);
                 this.feedback = err.message;
