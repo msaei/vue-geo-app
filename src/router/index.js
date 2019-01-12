12600fdbe77a54d8d9b5b25122d8c26795f7cc6e
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-12 11:15:30 -0500

    router gaurd for auth required pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,18 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [{
             path: '/',
             name: 'GMap',
-            component: GMap
+            component: GMap,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: '/signup',
@@ -24,3 +28,22 @@ export default new Router({
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    // check if route requires auth
+    if (to.matched.some(rec => rec.meta.requiresAuth)) {
+        //check auth state of user
+        let user = firebase.auth().currentUser
+        if (user) {
+            //if user signed in
+            next()
+        } else {
+            //redirect to login page
+            next({ name: 'Login' })
+        }
+    } else {
+        next()
+    }
+})
+
+export default router
