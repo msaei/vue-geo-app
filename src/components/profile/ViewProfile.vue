65f9acd2e2b44daa97f3bb68e73f31eb215b82cb
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-13 12:03:51 -0500

    style profile

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -72,7 +72,7 @@ export default {
         db.collection("comments")
           .add({
             to: this.$route.params.id,
-            from: this.user.id,
+            from: this.user.alias,
             content: this.newComment,
             time: Date.now()
           })
@@ -88,6 +88,18 @@ export default {
 </script>
 
 <style>
+.view-profile .card {
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2 {
+  font-size: 2em;
+  margin-top: 0px;
+}
+.view-profile li {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
 </style>
 
 
