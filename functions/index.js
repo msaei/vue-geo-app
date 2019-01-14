9a52cd8fb410800496011172ef0140b3061e16d8
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-14 13:57:46 -0500

    checkalias function added

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+admin.initializeApp()
+    // // Create and Deploy Your First Cloud Functions
+    // // https://firebase.google.com/docs/functions/write-firebase-functions
+    //
+    // exports.helloWorld = functions.https.onRequest((request, response) => {
+    //  response.send("Hello from Firebase!");
+    // });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.checkAlias = functions.https.onCall((data, context) => {
+
+    const ref = admin.firestore().collection('users').get(data.slug);
+    return ref.get().then(doc => {
+        return { uniqe: !doc.exists }
+    }).catch(err => {
+        throw new functions.https.HttpsError('failed to connect')
+    })
+})
